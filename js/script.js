f1cd0985d742d919f099e505258bf6417c02286f
aaocansey <albertocansey582@yahoo.com>
2022-09-14 12:21:49 +0000

    code scrambler files

---
+++ js/script.js
@@ -0,0 +1,47 @@
+const wordText = document.querySelector(".word"),
+hintText = document.querySelector(".hint span"),
+timeText = document.querySelector(".time b"),
+inputField = document.querySelector("input"),
+refreshBtn = document.querySelector(".refresh-word"),
+checkBtn = document.querySelector(".check-word");
+
+let correctCode, timer;
+
+const initTimer = maxTime => {
+    clearInterval(timer);
+    timer = setInterval(() => {
+        if(maxTime > 0) {
+            maxTime--;
+            return timeText.innerText = maxTime;
+        }
+        alert(`Time off! ${correctCode.toLowerCase()} was the correct code`);
+        initGame();
+    }, 1000);
+}
+
+const initGame = () => {
+    initTimer(30);
+    let randomObj = words[Math.floor(Math.random() * words.length)];
+    let wordArray = randomObj.word.split("");
+    for (let i = wordArray.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [wordArray[i], wordArray[j]] = [wordArray[j], wordArray[i]];
+    }
+    wordText.innerText = wordArray.join("");
+    hintText.innerText = randomObj.hint;
+    correctCode = randomObj.word.toLowerCase();;
+    inputField.value = "";
+    inputField.setAttribute("maxlength", correctCode.length);
+}
+initGame();
+
+const checkWord = () => {
+    let userWord = inputField.value.toLowerCase();
+    if(!userWord) return alert("Please enter the code to check!");
+    if(userWord !== correctCode) return alert(`Oops! ${userWord} is not a correct code`);
+    alert(`Congrats! ${correctCode.toLowerCase()} is the correct code`);
+    initGame();
+}
+
+refreshBtn.addEventListener("click", initGame);
+checkBtn.addEventListener("click", checkWord);
---
+++ js/words.js
@@ -0,0 +1,30 @@
+let words = [
+    {
+        word: "<meta>",
+        hint: "An HTML tag found in the head tag"
+    },
+    {
+        word: "<blockquote>",
+        hint: "Used to define a large quoted section"
+    },
+    {
+        word: "<button name='button' type='button'></button>",
+        hint: "An element used to submit forms and actions"
+    },
+    {
+        word: '<link rel="stylesheet" type="text/css" href="mystyle.css" />',
+        hint: "Used to link css files"
+    },
+    {
+        word: "p{margin:20px}",
+        hint: "A line of code to give a paragraph 20px space from another element"
+    }
+    {
+        word: '<strong>',
+        hint: "Used highlight important text"
+    },
+    {
+        word: "p{margin:20px}",
+        hint: "A line of code to give a paragraph 20px space from another element"
+    }
+]
